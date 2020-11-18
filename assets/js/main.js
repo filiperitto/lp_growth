e37798e7ba69997dee62d24e3fab3a8a4332f6a1
Filipe Ritto <filipe.ritto@gmail.com>
2020-11-18 12:50:00 -0300

    Modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ var lightboxInlineIframe = GLightbox({
 
 
 
-//== /Accordion Module
+//== Accordion Module
 (function(){
     let accTitle = document.getElementsByClassName("acc-heading");
     let accContent = document.getElementsByClassName("acc-content");
@@ -134,5 +134,21 @@ var lightboxInlineIframe = GLightbox({
 //======================== /Accordion Module
         
 
+//== Modal Toggle
+function modalSampleToggle() {
+    document.getElementById('modalSample').classList.toggle('open');
+    document.getElementById('inv-modal').classList.toggle('open');
+    document.getElementById('body').classList.toggle('scrollhidden');
+}
+// Select all the elements with example class.
+var modalSample = document.querySelectorAll('.modalcta');
+// Loop through the elements.
+for (var i = 0; i < modalSample.length; i++) {
+// Add the class margin to the individual elements.
+modalSample[i].addEventListener('click', modalSampleToggle);
+}
+//------------------------ Modal Header
+
+new ClipboardJS('.btn-copy');
 
 
